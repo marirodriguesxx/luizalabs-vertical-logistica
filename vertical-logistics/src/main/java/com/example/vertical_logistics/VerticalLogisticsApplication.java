d18d378cfc7920e6f0d1e33665b7202c6238bb71
marirodriguesxx <marianarodriguesdesantana@gmail.com>
2024-06-19 10:23:18 -0300

    spring boot project base

---
+++ vertical-logistics/src/main/java/com/example/vertical_logistics/VerticalLogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.vertical_logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerticalLogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerticalLogisticsApplication.class, args);
+	}
+
+}
---
+++ vertical-logistics/src/test/java/com/example/vertical_logistics/VerticalLogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vertical_logistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerticalLogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
