c478bcafc965d090d211080fbc2a0762db83e3ad
marirodriguesxx <marianarodriguesdesantana@gmail.com>
2024-06-22 17:59:50 -0300

    definindo controller

---
+++ vertical-logistics/src/main/java/com/example/vertical_logistics/adapter/in/web/FileUploadController.java
@@ -0,0 +1,48 @@
+package com.example.vertical_logistics.adapter.in.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import com.example.vertical_logistics.application.port.in.FileUploadUseCase;
+import com.example.vertical_logistics.domain.model.User;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/files")
+public class FileUploadController {
+
+    private final FileUploadUseCase fileUploadUseCase;
+
+    public FileUploadController(FileUploadUseCase fileUploadUseCase) {
+        this.fileUploadUseCase = fileUploadUseCase;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            fileUploadUseCase.uploadFile(file);
+            return ResponseEntity.status(201).body("File uploaded successfully.");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(400).body(e.getMessage());
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body("Failed to upload file due to an I/O error.");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Failed to upload file due to an unexpected error.");
+        }
+    }
+    @GetMapping("/orders")
+    public ResponseEntity<List<User>> getOrders(@RequestParam(required = false) Integer orderId,
+                                                @RequestParam(required = false) String startDate,
+                                                @RequestParam(required = false) String endDate) {
+        try {
+            List<User> orders = fileUploadUseCase.getOrders(orderId, startDate, endDate);
+            return ResponseEntity.ok(orders);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Collections.emptyList());
+        }
+    }
+}
