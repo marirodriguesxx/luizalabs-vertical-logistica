43d26047791c478bb03448f592a3fb5eeaeb54d7
marirodriguesxx <marianarodriguesdesantana@gmail.com>
2024-06-22 20:56:53 -0300

    comentando codigo desnecessario por agora

--- vertical-logistics/src/main/java/com/example/vertical_logistics/adapter/out/persistence/UserRepository.java
+++ vertical-logistics/src/main/java/com/example/vertical_logistics/adapter/out/persistence/UserRepository.java
@@ -2,19 +2,14 @@ package com.example.vertical_logistics.adapter.out.persistence;
 
 import com.example.vertical_logistics.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    @Query("SELECT u FROM User u JOIN u.orders o WHERE o.orderId = :orderId")
-    List<User> findByOrderId(@Param("orderId") Integer orderId);
-
-    @Query("SELECT u FROM User u JOIN u.orders o WHERE o.date BETWEEN :startDate AND :endDate")
-    List<User> findByDateRange(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+//    @Query("SELECT u FROM User u JOIN u.orders o WHERE o.orderId = :orderId")
+//    List<User> findByOrderId(@Param("orderId") Integer orderId);
+//
+//    @Query("SELECT u FROM User u JOIN u.orders o WHERE o.date BETWEEN :startDate AND :endDate")
+//    List<User> findByDateRange(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 }
