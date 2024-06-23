6dafe05759e5a843b4335b61f525f0b30ebe1e5a
marirodriguesxx <marianarodriguesdesantana@gmail.com>
2024-06-23 14:10:16 -0300

    consultando resultado atraves de consultas no banco de dados

--- vertical-logistics/src/main/java/com/example/vertical_logistics/adapter/in/web/UserController.java
+++ vertical-logistics/src/main/java/com/example/vertical_logistics/adapter/in/web/UserController.java
@@ -1,12 +1,14 @@
 package com.example.vertical_logistics.adapter.in.web;
 
 
+import com.example.vertical_logistics.application.dto.UserDTO;
+import com.example.vertical_logistics.application.mapper.UserMapper;
 import com.example.vertical_logistics.application.service.UserService;
 import com.example.vertical_logistics.domain.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -15,7 +17,6 @@ public class UserController {
 
     private final UserService userService;
 
-    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
@@ -26,5 +27,11 @@ public class UserController {
         Optional<User> user = userService.getUserById(id);
         return user.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/with-orders-and-products")
+    public ResponseEntity<List<UserDTO>> getUsersWithOrdersAndProducts() {
+        List<UserDTO> usersDTO = userService.findAllUsersWithOrdersAndProducts();
+        return ResponseEntity.ok(usersDTO);
     }
 }
